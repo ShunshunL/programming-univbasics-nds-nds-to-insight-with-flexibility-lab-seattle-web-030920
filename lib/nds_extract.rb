c1b122f72ca11d26c291637923eb1321d65e0f45
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-07 01:30:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -85,7 +85,13 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-  pp source
+  new_array = []
+  index = 0 
+  while index < source.length do 
+    new_array << movies_with_director_key(source[index][name], source[index][:movies])
+    index += 1 
+  end
+  new_array
 end
 
 # ----------------    End of Your Code Region --------------------
