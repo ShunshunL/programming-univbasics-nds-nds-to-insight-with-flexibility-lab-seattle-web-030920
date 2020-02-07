2fc739e6a27217ebe5b77cf1ac37991e76aab853
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-07 01:34:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -72,6 +72,11 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
   pp collection
+  gross = {}
+  index = 0 
+  while index < collection.length do 
+    
+  end
 end
 
 def movies_with_directors_set(source)
